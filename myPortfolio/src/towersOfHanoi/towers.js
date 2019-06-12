fbc0dec19235f910a3e02ecf8486dc04dfed700c
jaywiles <jay.wiles@gmail.com>
2019-06-12 09:42:49 -0500

    fix after tutoring

--- myPortfolio/src/towersOfHanoi/towers.js
+++ myPortfolio/src/towersOfHanoi/towers.js
@@ -1,14 +1,24 @@
 'use strict';
 
 $(document).ready(function() {
-  let $block = null;
+  let block = null;
+  let pickUp = null;
+  let dropOff = null;
   
   $('[tower]').click(function() {
-    if ($block) {
-      $(this).append($block);
-      $block = null;
+    // gets block's value
+    if(block) {
+      dropOff = $(this).children().last().data("value");
     } else {
-      $block = $(this).children().last().detach();
+      pickUp = $(this).children().last().data("value");
+    }
+
+    // moves block
+    if ((block) && (pickUp < dropOff) || (block) && (dropOff === undefined)) {
+      $(this).append(block);
+      block = null;
+    } else if (block === null) {
+      block = $(this).children().last().detach();
     }
   })
 })
